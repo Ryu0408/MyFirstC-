324e959c88a4e27266e7c15f8998aeeb6f0daf4c
Ryu0408 <fbtmdghks11@naver.com>
2020-01-18 02:59:09 +0900

    C++ 출력방법

--- FirstCPP/FirstCPP/HelloWord.cpp
+++ FirstCPP/FirstCPP/HelloWord.cpp
@@ -1,1 +1,15 @@
-#
+#include <iostream>
+//C++의 표준 헤더파일은 iostream.h
+//단, c++에서는 표준 헤더파일의 확장자를 생략한다.
+
+int main(void) {
+	int num = 20;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello "<< "World!" <<std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+	system("pause");
+	return 0;
+
+
+}
