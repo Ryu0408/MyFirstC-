77ee31edbf7b96f33b9e56cd854a047700d180bc
Ryu0408 <fbtmdghks11@naver.com>
2020-02-01 07:23:34 +0900

    생성자를 활용한 맴버 이니셜라이저의 호출(생성자 선언 후 나중에 변수 대입)

---
+++ FirstCPP/FirstCPP/Constructor2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+private:
+	const int n1;
+	int n2;
+
+public:
+	Test(int num1, int num2)
+		:n1(num1), n2(num2) {
+		cout << "실행문 없음" << endl;
+	}
+	void ShowDate() {
+		cout << n1 << ', ' << n2 << endl;
+	}
+};
+
+int main(void) {
+	Test t1(10, 20);
+	t1.ShowDate();
+	system("pause");
+	return 0;
+}
